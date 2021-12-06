84c888eabb664f3cbd8c9181c9cd696fcaf91f3f
Jackson Figueiredo <jacksonsmfigueiredo@hotmail.com>
2021-12-06 19:07:29 -0300

    Returning only necessary fields inside DTO

--- DTOs/HackerNewStoryDTO.cs
+++ DTOs/HackerNewStoryDTO.cs
@@ -8,13 +8,10 @@ namespace Hacker_News_API.DTOs
     public class HackerNewStoryDTO
     {
         public string by { get; set; }
-        public int descendants { get; set; }
-        public int id { get; set; }
         public List<int> kids { get; set; }
         public int score { get; set; }
         public DateTime time { get; set; }
         public string title { get; set; }
-        public string type { get; set; }
         public string url { get; set; }
     }
 }
